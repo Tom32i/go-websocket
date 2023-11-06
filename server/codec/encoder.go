ff538704d1344550cec89ddd667c68219eca4720
Thomas Jarrand <thomas.jarrand@bedrockstreaming.com>
2023-11-06 17:12:25 +0100

    Use pointer + format

--- server/codec/encoder.go
+++ server/codec/encoder.go
@@ -1,67 +1,67 @@
 package codec
 
 import (
-    // "log"
-    "bytes"
+	// "log"
+	"bytes"
 )
 
 type BinaryEncoder struct {
-    idCodec Codec
-    codecsByName map[string]RegisteredCodec
-    codecsById map[uint8]RegisteredCodec
+	idCodec      Codec
+	codecsByName map[string]*RegisteredCodec
+	codecsById   map[uint8]*RegisteredCodec
 }
 
 type Codec interface {
-    encode(buffer *bytes.Buffer, data any)
-    decode(buffer *bytes.Buffer) any
+	encode(buffer *bytes.Buffer, data any)
+	decode(buffer *bytes.Buffer) any
 }
 
 type RegisteredCodec struct {
-    Id uint8
-    Name string
-    Handler Codec
+	Id      uint8
+	Name    string
+	Handler Codec
 }
 
 type Message struct {
-    Name string
-    Data any
+	Name string
+	Data any
 }
 
-func CreateBinaryEncoder(codecs []RegisteredCodec, idCodec Codec) BinaryEncoder {
-    codecsByName := make(map[string]RegisteredCodec)
-    codecsById := make(map[uint8]RegisteredCodec)
+func CreateBinaryEncoder(codecs []*RegisteredCodec, idCodec Codec) BinaryEncoder {
+	codecsByName := make(map[string]*RegisteredCodec)
+	codecsById := make(map[uint8]*RegisteredCodec)
 
-    for index, codec := range codecs {
-        codec.Id = uint8(index)
-        codecsById[codec.Id] = codec
-        codecsByName[codec.Name] = codec
-    }
+	for index, codec := range codecs {
+		codec.Id = uint8(index)
+		codecsById[codec.Id] = codec
+		codecsByName[codec.Name] = codec
+	}
 
-    return BinaryEncoder{
-        idCodec: idCodec,
-        codecsByName: codecsByName,
-        codecsById: codecsById,
-    }
+	return BinaryEncoder{
+		idCodec:      idCodec,
+		codecsByName: codecsByName,
+		codecsById:   codecsById,
+	}
 }
 
 func (e BinaryEncoder) Encode(name string, data any) []byte {
-    codec := e.codecsByName[name]
+	codec := e.codecsByName[name]
 
-    var buffer bytes.Buffer
+	var buffer bytes.Buffer
 
-    e.idCodec.encode(&buffer, codec.Id)
-    codec.Handler.encode(&buffer, data)
+	e.idCodec.encode(&buffer, codec.Id)
+	codec.Handler.encode(&buffer, data)
 
-    return buffer.Bytes()
+	return buffer.Bytes()
 }
 
 func (e BinaryEncoder) Decode(data []byte) Message {
-    var buffer = bytes.NewBuffer(data)
-    id := e.idCodec.decode(buffer);
-    codec := e.codecsById[id.(uint8)]
+	var buffer = bytes.NewBuffer(data)
+	id := e.idCodec.decode(buffer)
+	codec := e.codecsById[id.(uint8)]
 
-    return Message {
-        Name: codec.Name,
-        Data: codec.Handler.decode(buffer),
-    }
+	return Message{
+		Name: codec.Name,
+		Data: codec.Handler.decode(buffer),
+	}
 }
--- server/server.go
+++ server/server.go
@@ -1,153 +1,152 @@
 package server
 
 import (
-    "log"
-    "errors"
-    "net/http"
-    "github.com/gorilla/websocket"
-    "curvygo/server/codec"
+	"curvygo/server/codec"
+	"errors"
+	"github.com/gorilla/websocket"
+	"log"
+	"net/http"
 )
 
 type Server struct {
-    upgrader websocket.Upgrader
-    clients map[uint8]*Client
-    in chan ClientMessage
-    encoder codec.BinaryEncoder
+	upgrader websocket.Upgrader
+	clients  map[uint8]*Client
+	in       chan ClientMessage
+	encoder  codec.BinaryEncoder
 }
 
 func (server *Server) Handler(w http.ResponseWriter, r *http.Request) {
-    log.Printf("Upgrading connexion")
+	log.Printf("Upgrading connexion")
 
-    socket, err := server.upgrader.Upgrade(w, r, nil)
-    if err != nil {
-        log.Println(err)
-        return
-    }
+	socket, err := server.upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println(err)
+		return
+	}
 
-    client, error := server.createClient(socket)
+	client, error := server.createClient(socket)
 
-    if error != nil {
-        log.Println(error)
-        return
-    }
+	if error != nil {
+		log.Println(error)
+		return
+	}
 
-    server.init(client)
+	server.init(client)
 
-    go client.run(server)
+	go client.run(server)
 }
 
 func (server *Server) Run() {
-    log.Printf("Server running")
-    for {
-        select {
-            case m := <-server.in:
-                //log.Printf("message in: %v (%v)", m.message.Name, m.message.Data)
-                switch m.message.Name {
-                    case "me:name":
-                        m.client.setName(m.message.Data.(string))
-                        server.writeAll(
-                            "client:name",
-                            codec.ClientNameMessage { m.client.id, m.client.name },
-                        )
-                    case "me:position":
-                        position := m.message.Data.(codec.Position)
-                        m.client.setPosition(position.X, position.Y)
-                        server.writeAll(
-                            "client:position",
-                            codec.ClientPosition { m.client.id, codec.Position{m.client.x, m.client.y} },
-                        )
-                }
-        }
-    }
+	log.Printf("Server running")
+	for {
+		select {
+		case m := <-server.in:
+			//log.Printf("message in: %v (%v)", m.message.Name, m.message.Data)
+			switch m.message.Name {
+			case "me:name":
+				m.client.setName(m.message.Data.(string))
+				server.writeAll(
+					"client:name",
+					codec.ClientNameMessage{m.client.id, m.client.name},
+				)
+			case "me:position":
+				position := m.message.Data.(codec.Position)
+				m.client.setPosition(position.X, position.Y)
+				server.writeAll(
+					"client:position",
+					codec.ClientPosition{m.client.id, codec.Position{m.client.x, m.client.y}},
+				)
+			}
+		}
+	}
 }
 
 func (server *Server) createClient(socket *websocket.Conn) (*Client, error) {
-    id, error := server.nextId()
+	id, error := server.nextId()
 
-    if error != nil {
-        return nil, error
-    }
+	if error != nil {
+		return nil, error
+	}
 
-    c := Client{
-        id: id,
-        socket: socket,
-        encoder: server.encoder,
-    }
+	c := Client{
+		id:      id,
+		socket:  socket,
+		encoder: server.encoder,
+	}
 
-    server.clients[c.id] = &c
+	server.clients[c.id] = &c
 
-    log.Printf("Client #%d joined.", c.id)
+	log.Printf("Client #%d joined.", c.id)
 
-    return &c, nil
+	return &c, nil
 }
 
 func (server *Server) removeClient(c *Client) {
-    delete(server.clients, c.id)
-    server.writeAll("client:remove", c.id)
-    log.Printf("Client #%d left.", c.id)
+	delete(server.clients, c.id)
+	server.writeAll("client:remove", c.id)
+	log.Printf("Client #%d left.", c.id)
 }
 
 func (server Server) writeAll(name string, data any) {
-    buffer := server.encoder.Encode(name, data)
-    for _, c := range server.clients {
-        c.write(buffer)
-    }
+	buffer := server.encoder.Encode(name, data)
+	for _, c := range server.clients {
+		c.write(buffer)
+	}
 }
 
 func (server *Server) init(client *Client) {
-    // Send the client to everybody
-    server.writeAll("client:add", codec.ClientAddMessage { client.id, client.name })
+	// Send the client to everybody
+	server.writeAll("client:add", codec.ClientAddMessage{client.id, client.name})
 
-    // Send the client its id
-    client.write(server.encoder.Encode("me:id", client.id))
+	// Send the client its id
+	client.write(server.encoder.Encode("me:id", client.id))
 
-    // Send the clients the current client list and positions
-    for _, c := range server.clients {
-        if c.id != client.id {
-            client.write(server.encoder.Encode("client:add", codec.ClientAddMessage { c.id, c.name }))
-            client.write(server.encoder.Encode("client:position", codec.ClientPosition { c.id, codec.Position{c.x, c.y} }))
-        }
-    }
+	// Send the clients the current client list and positions
+	for _, c := range server.clients {
+		if c.id != client.id {
+			client.write(server.encoder.Encode("client:add", codec.ClientAddMessage{c.id, c.name}))
+			client.write(server.encoder.Encode("client:position", codec.ClientPosition{c.id, codec.Position{c.x, c.y}}))
+		}
+	}
 }
 
 func (server *Server) nextId() (uint8, error) {
-    for i := 0; i < 256; i++ {
-        id := uint8(i)
-        _, ok := server.clients[id]
+	for i := 0; i < 256; i++ {
+		id := uint8(i)
+		_, ok := server.clients[id]
 
-        if !ok {
-            return id, nil
-        }
-    }
+		if !ok {
+			return id, nil
+		}
+	}
 
-    return uint8(0), errors.New("Client limit reached!")
+	return uint8(0), errors.New("Client limit reached!")
 }
 
 func CreateServer() Server {
-    return Server{
-        clients: make(map[uint8]*Client),
-        in: make(chan ClientMessage),
-        upgrader: websocket.Upgrader{
-            ReadBufferSize:  1024,
-            WriteBufferSize: 1024,
-            Subprotocols: []string{"websocket"},
-            Error: func (w http.ResponseWriter, r *http.Request, status int, reason error) {
-                log.Printf("Error: %v %v", status, reason)
-            },
-            CheckOrigin: func (r *http.Request) bool {
-                return true
-            },
-        },
-        encoder: codec.CreateBinaryEncoder([]codec.RegisteredCodec{
-            {0, "me:id", codec.Int8Codec{}},
-            {0, "me:name", codec.StringCodec{}},
-            {0, "me:position", codec.CreatePositionCodec()},
-            {0, "client:add", codec.CreateClientAddCodec()},
-            {0, "client:remove", codec.Int8Codec{}},
-            {0, "client:name", codec.CreateClientNameCodec()},
-            {0, "client:position", codec.CreateClientPositionCodec()},
-            {0, "say", codec.StringCodec{}},
-        }, codec.Int8Codec{}),
-    }
+	return Server{
+		clients: make(map[uint8]*Client),
+		in:      make(chan ClientMessage),
+		upgrader: websocket.Upgrader{
+			ReadBufferSize:  1024,
+			WriteBufferSize: 1024,
+			Subprotocols:    []string{"websocket"},
+			Error: func(w http.ResponseWriter, r *http.Request, status int, reason error) {
+				log.Printf("Error: %v %v", status, reason)
+			},
+			CheckOrigin: func(r *http.Request) bool {
+				return true
+			},
+		},
+		encoder: codec.CreateBinaryEncoder([]*codec.RegisteredCodec{
+			{0, "me:id", codec.Int8Codec{}},
+			{0, "me:name", codec.StringCodec{}},
+			{0, "me:position", codec.CreatePositionCodec()},
+			{0, "client:add", codec.CreateClientAddCodec()},
+			{0, "client:remove", codec.Int8Codec{}},
+			{0, "client:name", codec.CreateClientNameCodec()},
+			{0, "client:position", codec.CreateClientPositionCodec()},
+			{0, "say", codec.StringCodec{}},
+		}, codec.Int8Codec{}),
+	}
 }
-
