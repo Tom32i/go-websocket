8aab029840feacb2c98e187d87fde014a28d6486
Thomas Jarrand <thomas.jarrand@bedrockstreaming.com>
2023-11-21 17:42:41 +0100

    Simplification

--- server/codec/int16.go
+++ server/codec/int16.go
@@ -11,10 +11,10 @@ type Int16Codec struct {
 
 func (c Int16Codec) encode(buffer *bytes.Buffer, data any) {
     b := make([]byte, 2)
-    binary.BigEndian.PutUint16(b[0:], data.(uint16))
+    binary.BigEndian.PutUint16(b, data.(uint16))
     buffer.Write(b)
 }
 
 func (c Int16Codec) decode(buffer *bytes.Buffer) any {
-    return binary.BigEndian.Uint16(buffer.Next(2)[0:])
+    return binary.BigEndian.Uint16(buffer.Next(2))
 }
